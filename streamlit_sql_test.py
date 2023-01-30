e5dfd0487d377b178a714d7fb2180fa0cab19407
daltonkraklan <109905866+daltonkraklan@users.noreply.github.com>
2023-01-30 13:47:11 -0500

    Add files via upload

---
+++ streamlit_sql_test.py
@@ -0,0 +1,30 @@
+
+import streamlit as st
+import pandas as pd
+import sqlalchemy
+engine = sqlalchemy.create_engine('mysql+pymysql://root:9U7s0GjX%*dH*aQI3Bmf@localhost:3306/nba_api')
+
+st.set_page_config(
+    page_title = 'Test',
+    layout = 'wide'
+
+)
+
+def pd_to_sql(df,table):
+    df.columns = df.columns.str.strip()
+    df.to_sql(
+        name = table,
+        con = engine,
+        index = False,
+        if_exists = 'replace'
+    )
+
+def pd_read_sql(table):
+    df = pd.read_sql(table , engine)
+    return df
+
+df = pd_read_sql('today_preds')
+
+st.header('hello')
+
+st.dataframe(df)
